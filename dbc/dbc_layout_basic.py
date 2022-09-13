72dcb26d4dbf166c37a6104bc91879358f538dc7
Mena WANG <ningwang25@gmail.com>
2022-09-13 18:28:46 +1000

    put files in folders

---
+++ dbc/dbc_layout_basic.py
@@ -0,0 +1,16 @@
+from dash import Dash, html
+import dash_bootstrap_components as dbc
+
+app = Dash(external_stylesheets=[dbc.themes.FLATLY])
+
+app.layout = dbc.Container([
+    html.P("This is above the alert"),
+    dbc.Alert("Hello Bootstrap!", color="success")],
+    # className cheatsheet: https://dashcheatsheet.pythonanywhere.com/
+    className="p-3 bg-danger bg-opacity-50 text-white",
+)
+
+if __name__ == "__main__":
+    app.run_server(debug = True,
+                    host = '0.0.0.0',
+                    port = 5000)
